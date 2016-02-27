60d68f468b078748e07db446f191ba5addc494bf
July Tran <julyytran@gmail.com>
2016-02-27 15:57:27 -0700

    complete create and got gifs working
    cool

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,12 +1,30 @@
+require 'apicall'
+
 class GifsController < ApplicationController
 
   def index
 
   end
 
+  def show
+    @gif = Gif.find(params[:id])
+    @category = @gif.category
+  end
+
   def new
-    @category = Category.new
     @gif = Gif.new
   end
 
+  def create
+    @search_term = params[:search_term]
+    @image_path = APICall.image_path
+    @gif = Gif.new(image_path: @image_path)
+    if @gif.save
+      @category = Category.where(name: @search_term).first_or_create
+      @category.gifs << @gif
+      redirect_to @gif
+    else
+      render :new
+    end
+  end
 end
--- spec/helpers.rb
+++ spec/helpers.rb
@@ -10,12 +10,4 @@ module Helpers
     fill_in "Password", with: "password"
     click_button "Login"
   end
-
-  def image_path_one
-    response = Faraday.get("http://api.giphy.com/v1/gifs/search?q=funny+cat&limit=1&api_key=dc6zaTOxFJmzC")
-    data = JSON.parse(response.body, object_class: OpenStruct)
-    rull_data = data.data
-    image_path = rull_data.first.url
-    return image_path
-  end
 end
